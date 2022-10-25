997bea1774c63719a64b40a1bf541aac0bd7087a
Romet Pastak <rometpastak@gmail.com>
2022-10-25 16:03:34 +0300

    Reacti alustus

---
+++ veebipood/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="main-pilt" src="https://mollerauto.ee/media/wysiwyg/MicrosoftTeams-image_18__2.png" alt="" />
+      <button className="nupp">Tere</button>
+    </div>
+  );
+}
+
+export default App;
